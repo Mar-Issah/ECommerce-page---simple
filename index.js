f1383e73b2ab1cab36fddbaffce2aab3cfea5770
Marsiya <67198163+Mar-Issah@users.noreply.github.com>
2021-04-04 11:34:15 -0400

    ecommerce proeject

---
+++ index.js
@@ -0,0 +1,384 @@
+const menu = [
+	{
+		id: 1,
+		title: "African Wear",
+		category: "women",
+		price: 40.99,
+		img: "/asset/women/african.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+	{
+		id: 2,
+		title: "Pink African Wear",
+		category: "women",
+		price: 30.99,
+		img: "/asset/women/african1.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+	{
+		id: 3,
+		title: "Blue Trouser",
+		category: "men",
+		price: 41.99,
+		img: "/asset/men/blue-trouser.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+
+	{
+		id: 4,
+		title: "Long Jumpsuit",
+		category: "women",
+		price: 35.99,
+		img: "/asset/women/jumpsuitLong.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+	{
+		id: 5,
+		title: "Short Jumpsuit",
+		category: "women",
+		price: 31.99,
+		img: "/asset/women/jumpsuitSh.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+	{
+		id: 6,
+		title: "Short Pink Jumpsuit",
+		category: "women",
+		price: 31.99,
+		img: "/asset/women/jumpsuit-short.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+	{
+		id: 7,
+		title: "Flair Wine Dress",
+		category: "women",
+		price: 33.99,
+		img: "/asset/women/red-dress.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+	{
+		id: 8,
+		title: "Long Jumpsuit",
+		category: "women",
+		price: 35.99,
+		img: "/asset/women/jumpsuit.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+	{
+		id: 9,
+		title: "Knitted Sweater",
+		category: "men",
+		price: 51.99,
+		img: "/asset/men/knitted-sweater.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+	{
+		id: 10,
+		title: "Casual Summer Shirt",
+		category: "men",
+		price: 29.99,
+		img: "/asset/men/light-shirt.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+	{
+		id: 11,
+		title: "Blue Jean",
+		category: "men",
+		price: 39.99,
+		img: "/asset/men/men-jean.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+
+	{
+		id: 12,
+		title: "Blue Jean",
+		category: "men",
+		price: 39.99,
+		img: "/asset/men/men-jeans.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+
+	{
+		id: 13,
+		title: "Polo Shirt",
+		category: "men",
+		price: 30.99,
+		img: "/asset/men/polo.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+
+	{
+		id: 14,
+		title: "Long Sleeve Shirt",
+		category: "men",
+		price: 38.99,
+		img: "/asset/men/wine-shirt.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+
+	{
+		id: 15,
+		title: "Dotted Shirt and Jean",
+		category: "kids",
+		price: 46.99,
+		img: "/asset/kids/kids.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+
+	{
+		id: 16,
+		title: "White and Orange Dress",
+		category: "kids",
+		price: 26.99,
+		img: "/asset/kids/kids1.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+
+	{
+		id: 17,
+		title: "Dotted Shorts and Top",
+		category: "kids",
+		price: 46.99,
+		img: "/asset/kids/kids2.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+
+	{
+		id: 18,
+		title: "Dotted Shorts and Top",
+		category: "kids",
+		price: 46.99,
+		img: "/asset/kids/kids3.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+
+	{
+		id: 19,
+		title: "White Lace Dress",
+		category: "kids",
+		price: 32.99,
+		img: "/asset/kids/kids4.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+	{
+		id: 20,
+		title: "Blue and White Dress",
+		category: "kids",
+		price: 32.99,
+		img: "/asset/kids/kids5.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+
+	{
+		id: 21,
+		title: "Pink Dress",
+		category: "kids",
+		price: 32.99,
+		img: "/asset/kids/kids6.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+
+	{
+		id: 22,
+		title: "Hair Gajra",
+		category: "accessories",
+		price: 12.99,
+		img: "/asset/accessories/hair.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+	{
+		id: 23,
+		title: "Wine Handbag",
+		category: "accessories",
+		price: 20.99,
+		img: "/asset/accessories/handbag.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+	{
+		id: 24,
+		title: "Ladies Watch",
+		category: "accessories",
+		price: 35.99,
+		img: "/asset/accessories/ladies-watchR.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+	{
+		id: 25,
+		title: "Ladies Watch Gold",
+		category: "accessories",
+		price: 40.99,
+		img: "/asset/accessories/ladies-watch.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+	{
+		id: 26,
+		title: "Men Watch",
+		category: "accessories",
+		price: 52.99,
+		img: "/asset/accessories/mens-watch.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+
+	{
+		id: 27,
+		title: "Princess Crown",
+		category: "accessories",
+		price: 22.99,
+		img: "/asset/accessories/queen-crown.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+
+	{
+		id: 28,
+		title: "Gold and Blue Ring",
+		category: "accessories",
+		price: 70.99,
+		img: "/asset/accessories/ring.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+
+	{
+		id: 29,
+		title: "Yellow Tote Handbag",
+		category: "accessories",
+		price: 25.99,
+		img: "/asset/accessories/tote.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+	{
+		id: 30,
+		title: "Yellow Tote Handbag",
+		category: "Others",
+		price: 25.99,
+		img: "/asset/accessories/tote.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+	{
+		id: 31,
+		title: "Shoping bag",
+		category: "Others",
+		price: 15.99,
+		img: "/asset/accessories/shopping-bag.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+	{
+		id: 32,
+		title: "Make-Up kits",
+		category: "Others",
+		price: 30.99,
+		img: "/asset/accessories/makeup kits.jpg",
+		desc:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrumquam corporis numquam officia doloribus omnis optio dolores dicta,tempora fugiat!",
+	},
+];
+
+const sectionCenter = document.querySelector(".section-center");
+const container = document.querySelector(".btn-container"); //container in which html buttons will be filled dynamically
+//const filterBtns = document.querySelectorAll(".filter-btn");  move down
+
+//load items
+window.addEventListener("DOMContentLoaded", () => {
+	displayMenuItems(menu);
+
+	displayMenuBtns();
+});
+
+// add all the objects dynamically with a function
+function displayMenuItems(menuItems) {
+	let displayMenu = menuItems.map((item) => {
+		return `<article class="menu-item">
+					<div class="img-container">
+						<img
+							src="${item.img}"
+							alt="${item.title}"
+							class="image"
+						/>
+					</div>
+					<div class="item-info">
+						<header>
+							<h4>${item.title}</h4>
+							<h4>${"$" + item.price}</h4>
+						</header>
+						<p class="item-text">
+							${item.desc}
+						</p>
+					</div>
+				</article>`;
+	});
+
+	displayMenu = displayMenu.join("");
+	sectionCenter.innerHTML = displayMenu;
+}
+
+//create button dynamically
+//returning each category name using te reduce fxn dynamically
+function displayMenuBtns() {
+	const categories = menu.reduce(
+		(values, item) => {
+			if (!values.includes(item.category)) {
+				values.push(item.category);
+			}
+			return values;
+		},
+		["all"]
+	);
+
+	const categoryBtns = categories
+		.map((category) => {
+			return `<button class="filter-btn" type="button" data-id="${category}">${category}</button>`;
+		})
+		.join("");
+	container.innerHTML = categoryBtns;
+	const filterBtns = document.querySelectorAll(".filter-btn");
+
+	//filter btns
+	////add event listener on btn, target the element id whch is also the category, filter the menu asking if the category id you click === the menu category if so return the item []
+	//if the categoty is all return every thing ie default else return the chosen items. displayMenuItems accepts an array as arguement
+	filterBtns.forEach((btn) => {
+		btn.addEventListener("click", (e) => {
+			const category = e.currentTarget.dataset.id;
+			const menuCategory = menu.filter((menuItem) => {
+				if (menuItem.category == category) {
+					return menuItem;
+				}
+			});
+
+			if (category == "all") {
+				displayMenuItems(menu);
+			} else {
+				displayMenuItems(menuCategory);
+			}
+		});
+	});
+}
